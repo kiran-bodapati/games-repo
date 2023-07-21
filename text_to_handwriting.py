645e93650857703adcafbf7186a7ca38e2c9df9c
kiran-bodapati <138106641+kiran-bodapati@users.noreply.github.com>
2023-07-22 00:08:21 +0530

    Add files via upload

---
+++ text_to_handwriting.py
@@ -0,0 +1,4 @@
+import pywhatkit as pw
+txt="""python is an interpreted language.I love learning python.It is easy to uderstand and apply in real aowrld use-cases"""
+pw.text_to_handwriting("txt","demo1.png")
+print("End")
